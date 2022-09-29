4fec031bf68d9a8110f2c9cbfdecb5ae499acfbf
Suhogruzz <99946788+Suhogruzz@users.noreply.github.com>
2022-09-29 08:36:24 +0300

    Update settings.py

--- smart_home/smart_home/settings.py
+++ smart_home/smart_home/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_smart_home',
         'USER': 'postgres',
-        'PASSWORD': 'ikzgybx',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
